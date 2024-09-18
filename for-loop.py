d59b088d5acd8ccd2291118a89b63fa7e57494e2
byAMC31 <adrianmtzcl@gmail.com>
2024-09-18 02:23:14 +0000

    Ejercicios realizados en AWS - Cloud9

--- for-loop.py
+++ for-loop.py
@@ -6,10 +6,11 @@ print("Count to 10!")
 #Python utiliza la sangría para determinar que la instrucción print está dentro de la instrucción del bucle for
 for x in range (0, 11):
     print(x)
-
+ 
 
 # La instrucción for utiliza las palabras clave for … in para indicar a la computadora que recorra la lista.
-#La función range() genera una lista. La función range() toma un número inicial y un número final,
+#La función range() genera una lista.
+#La función range() toma un número inicial y un número final,
 #pero el número final no está incluido. Por lo tanto, pasa 11 para que la función deje de contar en 10.
 #La letra x actúa como una variable. 
 #Cada vez que se ejecuta el bucle, la variable x se asigna a la siguiente variable en el bucle y se muestra en la pantalla.
